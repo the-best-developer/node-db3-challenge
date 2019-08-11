cdefb664be20508976e9291e37b6223f0a5b5bd5
the-best-developer <im.dylan.gosnell@gmail.com>
2019-08-11 15:48:07 -0400

    Added findSteps by scheme ID

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,7 +4,7 @@ const find = _ => {
       return db('schemes');
 }
 
-const findById = async(id) => {
+const findById = async (id) => {
     try{
         const selectedScheme = await db('schemes').where({ id }).first();
         return (selectedScheme) ? selectedScheme : null;
@@ -14,7 +14,24 @@ const findById = async(id) => {
     }
 }
 
+const findSteps = async (id) => {
+    const selectedStep = await
+        // Select scheme table and alias as sc
+        db('schemes as sc')
+        // Join steps table row when scheme ID matches scheme_id in steps table
+        .join('steps as st', 'sc.id', '=', 'st.scheme_id')
+        // Build table using steps and schemem data together
+        .select('st.id', 'sc.scheme_name', 'st.step_number', 'st.instructions')
+        // Only select steps that match the given scheme
+        .where({ scheme_id: id } )
+        // Sort by step_number, in ascending order
+        .orderBy('st.step_number', 'asc');
+        // If everything checks out, return array
+        return (selectedStep) ? selectedStep : null;
+}
+
 module.exports = {
     find,
-    findById
+    findById,
+    findSteps
   }
